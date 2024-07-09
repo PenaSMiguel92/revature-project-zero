e0d593987d5ed1ffec1cf05a3fbe6ed826915000
Miguel Pena <miguel674@revature.net>
2024-07-08 22:49:12 -0700

    added data classification references to BMI and BGC

--- project-env/implementation/bgc_data_handler.py
+++ project-env/implementation/bgc_data_handler.py
@@ -3,7 +3,9 @@ from interface.data_handler_interface import DataHandlerInterface
 class BGC_DataHandler(DataHandlerInterface):
     """
         BGC -> Blood Glucose Concentration
-        
+        Random -> Normal: < 200 mg/dl, Prediabetes: N/A, Diabetes: >= 200 mg/dl
+        Fasting -> Normal: < 100 mg/dl, Prediabetes: 100 - 125 mg/dl, Diabetes: >= 126 mg/dl
+        2 hr after eating -> Normal: < 140 mg/dl, Prediabetes: 140 - 199 mg/dl, Diabetes: >= 200 mg/dl 
     """
     def __init__(self):
         pass
--- project-env/implementation/bmi_data_handler.py
+++ project-env/implementation/bmi_data_handler.py
@@ -3,7 +3,16 @@ from interface.data_handler_interface import DataHandlerInterface
 class BMI_DataHandler(DataHandlerInterface):
     """
         BMI -> Body Mass Index
-        BMI = Mass (kg) / Height * Height (m)
+        BMI = Mass (kg) / Height * Height (m) or 703 x Weight (lbs) / Height * Height (inches)
+        WHO Classification:
+        Severe Thinness: < 16
+        Moderate Thinness: 16-17
+        Mild Thinness: 17 - 18.5
+        Normal: 18.5 - 25
+        Overweight: 25 - 30
+        Obese Class I: 30 - 35
+        Obese Class II: 35 - 40
+        Obese Class III: > 40
     """
     def __init__(self):
         pass
